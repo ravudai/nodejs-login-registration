4632da726a1970a48be21c67664e6ba38830204f
Renga Avudaiappan <ravudaiappan@gmail.com>
2021-05-03 07:54:20 +0200

    base version 2

---
+++ client/src/styles.tsx
@@ -0,0 +1,27 @@
+import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        height: '100vh',
+    },
+    paper: {
+        margin: theme.spacing(8,4),
+    },
+    mainContainer: {
+        backgroundImage: url(''),
+    }.
+    signinPanel: { 
+        padding: 10,
+        margin: '20px auto',
+        height: '60vh',
+        minHeight: '600px',
+        width: '20vw',
+        minWidth: '300px',
+    },
+    centerText: {
+        textAlign: 'center',
+    },
+}));
+
+export default useStyles;
